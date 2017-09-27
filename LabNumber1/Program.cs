06ed4822afdad6d2d3200d79e8016ee1e43749de
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-09-27 10:27:34 -0400

    This is Serena's first commit!

---
+++ LabNumber1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LAB NUMBER 1---- Serena Tindle
+
+            bool ContinueProgram = true; //Boolean Expression
+
+            while (ContinueProgram == true) //Insert While Loop
+            {
+                double RmLength, RmWidth; //Variables and Input needed
+                double RmArea = RmLength * RmWidth;
+                double RmPerimeter = 2 * (RmLength + RmWidth);
+
+                Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!"); //User Input
+                Console.WriteLine("Enter Length:");
+                RmLength = double.Parse(Console.ReadLine()); //Processing
+
+                Console.WriteLine("Enter Width:");
+                RmWidth = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Area:" + RmArea); //Output
+                Console.WriteLine("Perimeter:" + RmPerimeter);
+
+                string UserOption;
+                Console.WriteLine("Continue? (Y/N)");
+                UserOption = Console.ReadLine();
+                
+                if (UserOption == "N")
+                {
+                    ContinueProgram = false;
+                }
+       
+                Console.ReadKey();
+            }
+  
+        }
+    }
+}
